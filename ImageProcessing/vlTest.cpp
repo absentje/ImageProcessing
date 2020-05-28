a818b12b94247bcba74e9c8baf31b910172de00e
absentje <absent37@list.ru>
2020-05-28 07:58:16 +0300

    add vlTest executable

---
+++ ImageProcessing/vlTest.cpp
@@ -0,0 +1,11 @@
+#include <vlCore/VisualizationLibrary.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	vl::VisualizationLibrary::init();
+	// test code
+	// ...
+	vl::VisualizationLibrary::shutdown();
+	return 0;
+}
