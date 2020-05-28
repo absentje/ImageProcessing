c8fd20646ce061bd357590923bd721299a2d5902
absentje <absent37@list.ru>
2020-05-28 10:24:18 +0300

    FBX SDK removed from 3rtparty folder

--- ImageProcessing/vlTest.cpp
+++ ImageProcessing/vlTest.cpp
@@ -1,9 +1,15 @@
 #include <vlCore/VisualizationLibrary.hpp>
 #include <iostream>
+#include <VLExtension/Util/FBX_Support/FbxResource.h>
 
 int main(int argc, char *argv[])
 {
 	vl::VisualizationLibrary::init();
+
+	FbxResource fbxResource;
+	fbxResource.LoadFile("resources/fbx/Sc2.fbx");
+
+	auto geoms = fbxResource.GetGeometries();
 	// test code
 	// ...
 	vl::VisualizationLibrary::shutdown();
